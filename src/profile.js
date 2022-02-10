5922c4fad7f26700879fff03001f7e148842f143
desertcoqui <fdetres@my365.bellevue.edu>
2022-02-10 14:52:44 -0600

    hamburger navbar scripts run

--- src/profile.js
+++ src/profile.js
@@ -1,3 +1,9 @@
-const app = new Vue ({
 
-});
+
+let navbar = document.getElementById("popOutNavbar");
+navbar.style.display = "none";
+
+function hamburger(){
+    
+    navbar.style.display= "block";
+}
